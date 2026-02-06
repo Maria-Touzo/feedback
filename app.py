b2aefb5dfd25821e5ed5062cf3ae6c29d139273a
Maria-Touzo <maria.touzo@aluno.senai.br>
2026-02-06 15:48:22 -0300

    projeto finalizado até agora

--- app.py
+++ app.py
@@ -41,4 +41,5 @@ def adicionar_comentario():
     print(lista_de_comentario)
     return redirect("/comentarios")
 
-app.run(debug=True)
+if __name__  == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug= True)
